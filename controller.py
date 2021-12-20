0b7f31b20e6d1dfaa318429d3dd7d4a8ac0bbd30
WilliamArnone <arnonewilliam@gmail.com>
2021-12-20 21:45:43 +0100

    Moved properties check logic

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 from graphviz import Source 
 import copy
+from dot_parser.Graph import Graph
 from dot_parser.main import main as parse
 
 class Controller:
@@ -21,8 +22,8 @@ class Controller:
         log = []
         if properties['SP'].get():
             log.append('SP - Square Property:')
-            err = []
-            self.graph.CheckSP(err)
+            err = set()
+            self.CheckSP(self.graph, err)
             if err:
                 for error in err:
                     start, label, end, is_forward = error
@@ -32,33 +33,37 @@ class Controller:
                         log.append('Can\'t find '+EdgeToString(error))
             else:
                 log.append('SP holds')
+
         if properties['BTI'].get():
             log.append('BTI - Backward Transitions are Indipendent:')
-            err = []
-            self.graph.CheckBTI(err)
+            err = set()
+            self.CheckBTI(self.graph, err)
             if err:
                 for edge1, edge2 in err:
                     log.append(EdgeToString(edge1)+' and '+EdgeToString(edge2)+' are not indipendent')
             else:
                 log.append('BTI holds')
+
         if properties['WF'].get():
             log.append('WF - Well-Foundedness:')
-            err = []
-            self.graph.CheckWF(err)
+            err = set()
+            self.CheckWF(self.graph, err)
             if err:
                 for error in err:
                     log.append(EdgeToString(error)+' creates a cycle')
             else:
                 log.append('WF holds')
+
         if properties['CPI'].get():
             log.append('CPI - Coinitial Propagation of Indipendence:')
-            err = []
-            self.graph.CheckCPI(err)
+            err = set()
+            self.CheckCPI(self.graph, err)
             if err:
                 for edge1, edge2 in err:
                     log.append(EdgeToString(edge1)+' and '+EdgeToString(edge2)+' are not indipendent')
             else:
                 log.append('CPI holds')
+
         return log
                 
     def generateProperties(self, properties):
@@ -68,7 +73,7 @@ class Controller:
             log = []
             if properties['SP'].get():
                 err = []
-                new_graph.CheckSP(err)
+                self.CheckSP(new_graph, err)
                 if err:
                     for error in err:
                         start, label, end, forward = error
@@ -76,7 +81,7 @@ class Controller:
                         
             if properties['BTI'].get():
                 err = set()
-                new_graph.CheckBTI(err)
+                self.CheckBTI(new_graph, err)
                 if err:
                     for error in err:
                         edge1, edge2 = error
@@ -84,19 +89,122 @@ class Controller:
                         
             if properties['WF'].get():
                 err = set()
-                new_graph.CheckWF(err)
+                self.CheckWF(new_graph, err)
                 if err:
                     for error in err:
                         new_graph.RemoveEdge(error)
                         
             if properties['CPI'].get():
                 err = set()
-                new_graph.CheckCPI(err)
+                self.CheckCPI(new_graph, err)
                 if err:
                     for error in err:
                         edge1, edge2 = error
                         new_graph.AddIndipendence(edge1, edge2)
+
         return new_graph.ToString()
+
+    def CheckWF(self, graph:Graph, errors = None, node = None, visited=None):
+
+        if errors == None: errors = set()
+        if visited == None: visited = []
+        if node == None: node = graph.startNode
+
+        if node in visited: return False
+
+        visited.append(node)
+        for adj_node in graph.GetAdj(node):
+            if not self.CheckWF(graph, errors, adj_node, visited): 
+                for label in graph.GetEdgesBetween(node, adj_node, all=False):
+                    errors.add((node, label, adj_node, True))
+
+        visited.remove(node)
+
+        return len(errors)==0 if len(visited)==0 else True
+
+
+    def CheckSP(self, graph:Graph, errors = None):
+        if errors == None: errors = set()
+        for node in graph.nodes:
+            #remove all=False to check also backward transitions
+            edges = graph.GetEdgesFrom(node, all=False)
+            for edge1 in edges:
+                (start1, label1, end1, is_forward1) = edge1
+                for edge2 in edges:
+                    (start2, label2, end2, is_forward2) = edge2
+                    if graph.AreIndipendent(edge1, edge2):
+                        found = False
+                        firstForward = is_forward1 if (is_forward1==is_forward2) else not is_forward1
+                        secondForward = is_forward2 if (is_forward1==is_forward2) else not is_forward2
+                        for s in graph.nodes:
+                            first_start, first_end = (end1, s) if firstForward else (s, end1)
+                            second_start, second_end = (end2, s) if secondForward else (s, end2)
+
+                            first = (first_start, label2, first_end, firstForward)
+                            second = (second_start, label1, second_end, secondForward)
+
+                            firstExist = graph.EdgeExists(first) 
+                            secondExist = graph.EdgeExists(second)
+
+                            if firstExist or secondExist:
+                                found = True
+                                if not firstExist:
+                                    errors.add((end1, label2, s, firstForward))
+                                elif not secondExist:
+                                    errors.add((end2, label1, s, secondForward))
+
+                        if not found:
+                            errors.add((end1, label2, None, firstForward))
+                            errors.add((end2, label1, None, secondForward))
+
+                        
+        return len(errors)==0
+
+    def CheckBTI(self, graph:Graph, errors = None):
+        if errors == None: errors = set()
+        for node in graph.nodes:
+            edges = graph.GetEdgesFrom(node, all=False, only_forward=False)
+            for edge1 in edges:
+                for edge2 in edges:
+                    if edge1 != edge2 and not graph.AreIndipendent(edge1, edge2):
+                        errors.add((edge1, edge2))
+        return len(errors)==0
+
+    def CheckCPI(self, graph:Graph, errors):
+        if errors == None: errors = set()
+        for node in graph.nodes:
+            edges = graph.GetEdgesFrom(node, all=False)
+            for edge1 in edges:
+                (start1, label1, end1, is_forward1) = edge1
+                for edge2 in edges:
+                    #is this necessary?
+                    if edge1 == edge2: continue
+
+                    (start2, label2, end2, is_forward2) = edge2
+                    for s in graph.nodes:
+                        firstExist = graph.EdgeExists((end1, label2, s, True))
+                        secondExist = graph.EdgeExists((end2, label1, s, True))
+
+                        if firstExist and secondExist:
+                            cond = []
+                            temp = []
+                            cond.append((edge1, edge2))
+                            cond.append(((end1, label1, start1, False), 
+                                (end1, label2, s, True)))
+                            cond.append(((end2, label2, start2, False), 
+                                (end2, label1, s, True)))
+                            cond.append(((s, label2, end1, False), 
+                                (s, label1, end2, False)))
+
+                            for condition in cond:
+                                first, second = condition
+                                if not graph.AreIndipendent(first, second): temp.append(condition)
+
+                            if len(temp) > 0 and len(temp) < 4:
+                                for condition in temp:
+                                    errors.add(condition)
+                        
+        return len(errors)==0
 
 
 def EdgeToString(edge):
