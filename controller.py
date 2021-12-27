b87a8c508d1c284790f2ca441f9426f9415829a7
WilliamArnone <arnonewilliam@gmail.com>
2021-12-27 17:37:10 +0100

    indipendence printed and build moved in popup

--- controller.py
+++ controller.py
@@ -12,25 +12,29 @@ class Controller:
     graph = None
 
     def setPath(self, path):
+        """Set path oh the file to be read and parse the graph"""
         self.path = path
         graph, errors = parse(self.path)
         self.graph = graph
         return errors
 
     def getGraphImage(self):
+        """Save graph image and the return path to image"""
         s = Source.from_file(self.path)
         s.render(self.path, format='png',view=False)
         return self.path+'.png'
 
     def checkProperties(self, properties):
+        """Check graph properties and print error log"""
         log = []
+        errors = {}
         if properties['SP'].get():
+            errors['SP']=set()
             log.append('SP - Square Property:')
-            err = set()
-            if CheckSP(self.graph, err):
+            if CheckSP(self.graph, errors['SP']):
                 log.append('SP holds')
             else:
-                for error in err:
+                for error in errors['SP']:
                     start, label, end, is_forward = error
                     if end == None:
                         log.append(start+" should have a "+"forward" if is_forward else "backward" +" transiction labbeled "+label+" to a state")
@@ -38,90 +42,83 @@ class Controller:
                         log.append('Can\'t find '+EdgeToString(error))
 
         if properties['BTI'].get():
+            errors['BTI']=set()
             log.append('\nBTI - Backward Transitions are Indipendent:')
-            err = set()
-            if CheckBTI(self.graph, err):
+            if CheckBTI(self.graph, errors['BTI']):
                 log.append('BTI holds')
             else:
-                for edge1, edge2 in err:
+                for edge1, edge2 in errors['BTI']:
                     log.append(EdgeToString(edge1)+' and '+EdgeToString(edge2)+' are not indipendent')
 
         if properties['WF'].get():
+            errors['WF']=set()
             log.append('\nWF - Well-Foundedness:')
-            err = set()
-            if CheckWF(self.graph, err):
+            if CheckWF(self.graph, errors['WF']):
                 log.append('WF holds')
             else:
-                for error in err:
+                for error in errors['WF']:
                     log.append(EdgeToString(error)+' creates a cycle')
 
         if properties['CPI'].get():
+            errors['CPI']=set()
             log.append('\nCPI - Coinitial Propagation of Indipendence:')
-            err = set()
-            if CheckCPI(self.graph, err):
+            if CheckCPI(self.graph, errors['CPI']):
                 log.append('CPI holds')
             else:
-                for edge1, edge2 in err:
+                for edge1, edge2 in errors['CPI']:
                     log.append(EdgeToString(edge1)+' and '+EdgeToString(edge2)+' are not indipendent')
 
         if properties['IRE'].get():
+            errors['IRE']=set()
             log.append('\nIRE - Independence Respects Events:')
-            err = set()
-            if CheckIRE(self.graph, err):
+            if CheckIRE(self.graph, errors['IRE']):
                 log.append('IRE holds')
             else:
-                for edge1, edge2 in err:
+                for edge1, edge2 in errors['IRE']:
                     log.append(EdgeToString(edge1)+' and '+EdgeToString(edge2)+' are not indipendent')
 
-        return log
+        return log, errors
                 
-    def generateProperties(self, properties):
-        continue_to_check = True
-        new_graph = copy.deepcopy(self.graph)
-        while continue_to_check:
-            continue_to_check = False
-            if properties['SP'].get():
-                err = []
-                if not CheckSP(new_graph, err):
-                    continue_to_check = True
-                    for error in err:
-                        start, label, end, forward = error
-                        if(end != None): new_graph.AddEdge(error)
+    def generateProperties(self, graph, errors):
+        """Check graph properties and return a new graph with the selected properties"""
+        new_graph = copy.deepcopy(graph)
+        if 'SP' in errors:
+            for error in errors['SP']:
+                start, label, end, forward = error
+                if(end != None): new_graph.AddEdge(error)
                         
-            if properties['BTI'].get():
-                err = set()
-                if not CheckBTI(new_graph, err):
-                    continue_to_check = True
-                    for error in err:
-                        edge1, edge2 = error
-                        new_graph.AddIndipendence(edge1, edge2)
+        if 'BTI' in errors:
+            for error in errors['BTI']:
+                edge1, edge2 = error
+                new_graph.AddIndipendence(edge1, edge2)
                         
-            if properties['WF'].get():
-                err = set()
-                if not CheckWF(new_graph, err):
-                    continue_to_check = True
-                    for error in err:
-                        new_graph.RemoveEdge(error)
+        if 'WF' in errors:
+            for error in errors['WF']:
+                new_graph.RemoveEdge(error)
                         
-            if properties['CPI'].get():
-                err = set()
-                if not CheckCPI(new_graph, err):
-                    continue_to_check = True
-                    for error in err:
-                        edge1, edge2 = error
-                        new_graph.AddIndipendence(edge1, edge2)
+        if 'CPI' in errors:
+            for error in errors['CPI']:
+                edge1, edge2 = error
+                new_graph.AddIndipendence(edge1, edge2)
 
-            if properties['IRE'].get():
-                err = set()
-                if not CheckIRE(new_graph, err):
-                    continue_to_check = True
-                    for error in err:
-                        edge1, edge2 = error
-                        new_graph.AddIndipendence(edge1, edge2)
+        if 'IRE' in errors:
+            for error in errors['IRE']:
+                edge1, edge2 = error
+                new_graph.AddIndipendence(edge1, edge2)
 
         return new_graph.ToString()
+    
+    def GetIndipendenceString(self):
+        """Return indipendece between transition in a printable string"""
+        text = ""
+        for indipendence in self.graph.indipendence:
+            edge1, edge2 = indipendence
+            text = text+EdgeToString(edge1)+" ι "+EdgeToString(edge2)+"\n"
+        return text
+
 
 def EdgeToString(edge):
+    """Return a printable string of an edge"""
     start, label, end, is_forward = edge
     sign = '-' if is_forward else '~'
     return start+sign+label+sign+'>'+end
--- view.py
+++ view.py
@@ -1,8 +1,11 @@
+import copy
 import tkinter as ttk
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 from PIL import ImageTk, Image
+
+from controller import Controller
 
 class View:
     controller = None
@@ -24,6 +27,7 @@ class View:
         self.startUI()
 
     def startUI(self):
+        """Set up of the starting page"""
         self.clearScreen()
         self.root.geometry("350x250")
         self.root.grid_columnconfigure(0, weight=1)
@@ -40,72 +44,100 @@ class View:
         self.widgets.append(buttonExit)
 
     def mainUI(self, path):
+        """Set up the view with the graph and the properties"""
+
+        self.clearScreen()
+
+        if self.controller.path == None:
+
+            # window settings
+            self.root.geometry("1000x800")
+            self.root.grid_columnconfigure(0, weight=7)
+            self.root.grid_columnconfigure(1, weight=1)
+            self.root.grid_rowconfigure(0, weight=1)
+            self.root.grid_rowconfigure(1, weight=1)
+
+            properties_frame = LabelFrame(self.root, text = "Properties", padx=3, pady=3)
+            properties_frame.grid(row=0, column=1, sticky=W+E+S+N)
+
+        
+            frame_main = Frame(self.root, bg="gray")
+            frame_main.grid(row=1, column=1, sticky='news')
+
+            indipendenceLabel = Label(frame_main, text="INDIPENDENCE:")
+            indipendenceLabel.pack(fill="x")
+
+            canvas = Canvas(frame_main)
+            canvas.pack(side=LEFT, expand=YES, fill=BOTH)
+
+            scrollbar = Scrollbar(frame_main, command=canvas.yview)
+            scrollbar.pack(side=RIGHT, fill='y')
+
+            canvas.configure(yscrollcommand = scrollbar.set)
+
+            
+            # scrolling hanglers
+            canvas.bind('<Configure>', lambda event: canvas.configure(scrollregion=canvas.bbox('all')))
+            self.root.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(-1*(event.delta//120), "units"))
+
+            self.indipency_frame = Frame(canvas)
+            canvas.create_window((0,0), window=self.indipency_frame, anchor='nw')
+            
+            # initialize check buttons
+            properties = {}
+
+            properties['SP']=BooleanVar()
+            sp = Checkbutton(properties_frame, text="SP - Square Property", variable=properties['SP'])
+            sp.pack(anchor=W)
+
+            properties['BTI']=BooleanVar()
+            bti = Checkbutton(properties_frame, text="BTI - Backward Transitions are Indipendent", variable=properties['BTI'])
+            bti.pack(anchor=W)
+
+            properties['WF']=BooleanVar()
+            wf = Checkbutton(properties_frame, text="WF - Well-Foundedness", variable=properties['WF'])
+            wf.pack(anchor=W)
+
+            properties['CPI']=BooleanVar()
+            cpi = Checkbutton(properties_frame, text="CPI - Coinitial Propagation of Indipendence", variable=properties['CPI'])
+            cpi.pack(anchor=W)
+
+            properties['IRE']=BooleanVar()
+            ire = Checkbutton(properties_frame, text="IRE - Independence Respects Events", variable=properties['IRE'])
+            ire.pack(anchor=W)
+
+            btnCheck = Button(properties_frame, text="Check", command=lambda:self.checkProperties(properties))
+            btnCheck.pack()
+        
         errors = self.controller.setPath(path)
 
         if errors:
             messagebox.showerror("Error"," ".join(map(str, errors)))
             return
 
-        self.clearScreen()
-        self.root.geometry("1000x800")
-        self.root.grid_columnconfigure(0, weight=3)
-        self.root.grid_columnconfigure(1, weight=1)
-        self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_rowconfigure(1, weight=0)
-
+        # graph image
         imagePath = self.controller.getGraphImage()
         image = ImageTk.PhotoImage(Image.open(imagePath))
         lblImage = Label(image=image)
         lblImage.image=image
-        lblImage.grid(row=0, column=0, sticky=W+E+S+N)
+        lblImage.grid(row=0, column=0, rowspan=2, sticky=W+E+S+N)
         self.widgets.append(lblImage)
 
-        frame = LabelFrame(self.root, text = "Properties", padx=10, pady=10)
-        frame.grid(row=0, column=1, sticky=W+E+S+N)
-        self.widgets.append(frame)
+        txtIndipendence = Label(self.indipency_frame, border=0, justify=LEFT, text=self.controller.GetIndipendenceString())
+        txtIndipendence.pack(fill="both")
+        self.widgets.append(txtIndipendence)
 
-        properties = {}
-
-        properties['SP']=BooleanVar()
-        sp = Checkbutton(frame, text="SP - Square Property", variable=properties['SP'])
-        sp.pack(anchor=W)
-        self.widgets.append(sp)
-
-        properties['BTI']=BooleanVar()
-        bti = Checkbutton(frame, text="BTI - Backward Transitions are Indipendent", variable=properties['BTI'])
-        bti.pack(anchor=W)
-        self.widgets.append(bti)
-
-        properties['WF']=BooleanVar()
-        wf = Checkbutton(frame, text="WF - Well-Foundedness", variable=properties['WF'])
-        wf.pack(anchor=W)
-        self.widgets.append(wf)
-
-        properties['CPI']=BooleanVar()
-        cpi = Checkbutton(frame, text="CPI - Coinitial Propagation of Indipendence", variable=properties['CPI'])
-        cpi.pack(anchor=W)
-        self.widgets.append(cpi)
-
-        properties['IRE']=BooleanVar()
-        ire = Checkbutton(frame, text="IRE - Independence Respects Events", variable=properties['IRE'])
-        ire.pack(anchor=W)
-        self.widgets.append(ire)
-
-        btnCheck = Button(frame, text="Check", command=lambda:self.checkProperties(properties))
-        btnCheck.pack()
-        self.widgets.append(btnCheck)
-
-        btnBuild = Button(frame, text="Build", command=lambda:self.generateProperties(properties))
-        btnBuild.pack()
-        self.widgets.append(btnBuild)
+        
 
 
     def clearScreen(self):
+        """Destroy the start ui, or the image of the main ui"""
         for widget in self.widgets:
             widget.destroy()
         self.widgets = []
 
     def open_file(self):
+        """Open dot file and set the ui"""
         path = filedialog.askopenfilename(initialdir=".",
         		                              filetypes=(("DOT graph", "*.gv *.dot"), ("all files", "*.*")),
         		                              title="Choose a file."
@@ -114,13 +146,16 @@ class View:
         self.mainUI(path)
 
     def checkProperties(self, properties):
-        log = self.controller.checkProperties(properties)
+        """Check all properties and prints a log"""
+        log, errors = self.controller.checkProperties(properties)
         logString = ''
+        new_graph = copy.deepcopy(self.controller.graph)
 
         for msg in log: logString = logString + msg + '\n'
-        self.popUpWindow(logString)
+        self.popUpWindow(logString, new_graph, errors)
 
-    def popUpWindow(self, text):
+    def popUpWindow(self, text, graph, errors):
+        """Create a new window with the log infos"""
         root = Tk()
 
         canvas = Canvas(root)
@@ -131,36 +166,35 @@ class View:
 
         canvas.configure(yscrollcommand = scrollbar.set)
 
-        # update scrollregion after starting 'mainloop'
-        # when all widgets are in canvas
-        canvas.bind('<Configure>', lambda x: canvas.configure(scrollregion=canvas.bbox('all')))
-
-        # --- put frame in canvas ---
+        # scroll handling
+        canvas.bind('<Configure>', lambda event: canvas.configure(scrollregion=canvas.bbox('all')))
+        root.bind_all("<MouseWheel>", lambda event: canvas.yview_scroll(-1*(event.delta//120), "units"))
 
         frame = Frame(canvas)
         canvas.create_window((0,0), window=frame, anchor='nw')
 
-        # --- add widgets in frame ---
-
-        l = Label(frame, text=text, justify=LEFT, padx=15,pady=15)
+        l = Label(frame, text=text, justify=LEFT, border=0)
         l.pack(fill='both', expand=True)
 
         btn = Button(frame, text="Save", command=lambda: self.saveLog(text))
-        btn.pack()
+        btn.pack(pady=5)
 
-        # --- start program ---
+        btn = Button(frame, text="Apply", command=lambda: self.generateProperties(graph, errors))
+        btn.pack(pady=5)
 
         root.mainloop()
 
     def saveLog(self, text):
+        """Save text as a txt file"""
         dialog = filedialog.asksaveasfile(mode='w', title="Save Log", defaultextension=".txt", filetypes=(("Text FIle", "*.txt"), ("all files", "*.*")))
         if dialog is None: 
             return
         dialog.write(text)
         dialog.close() 
 
-    def generateProperties(self, properties):
-        text2save = self.controller.generateProperties(properties)
+    def generateProperties(self, graph, errors):
+        """Check properties and save a DOT file with the changes"""
+        text2save = self.controller.generateProperties(graph, errors)
         dialog = filedialog.asksaveasfile(mode='w', title="Save Graph", defaultextension=".dot", filetypes=(("DOT graph", "*.gv *.dot"), ("all files", "*.*")))
         if dialog is None: 
             return
